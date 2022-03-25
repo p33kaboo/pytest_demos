044cc6b857483008dce80cb5a4c74b557265d27f
tima <timuriuan@gmail.com>
2022-03-25 18:09:49 +0300

    added login page check

---
+++ pages/login_page.py
@@ -0,0 +1,22 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        login_page = self.browser.get(LoginPageLocators.LOGIN_URL)
+        assert 'login' in self.browser.current_url, f'Current url {self.browser.current_url} does not have "login"'
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), 'Error! Login form not found'
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), 'Error! Register form not found'
